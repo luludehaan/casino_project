e85d711131539a8d808db5df9ba8046dfabf6156
LuLu DeHaan <luludehaan@gmail.com>
2021-02-28 21:08:01 -0800

    Updated gemfile,  bet method

--- menu.rb
+++ menu.rb
@@ -7,14 +7,17 @@
 # Player places bet and wins / loses (hint: rand)
 # Player's bankroll goes up and down with wins and losses
 require_relative 'highlow'
+require 'colorize'
 
 def menu
-  
-  puts "welcome to the casino. What would you like to do?"
-  puts "1) Play Slots?"
-  puts "2) Play High Low?"
-  puts "3) how much have you lost?"
-  puts "4) Exit?"
+  puts "************   WELCOME TO THE CASINO   ************".yellow
+  puts
+  puts "Please make a selection.".cyan
+  puts
+  puts "1) Play Slots?".yellow
+  puts "2) Play High Low?".yellow
+  puts "3) See Balance.".yellow
+  puts "4) Exit?".yellow
   choice = gets.chomp.to_i
   case choice
   when 1
@@ -23,13 +26,13 @@ def menu
     high_low_menu
   when 3 
     #bankroll
-    puts "You have $#{@amount.to_f.round(3)} to bet with."
+    puts "You have $#{@amount.to_f.round(3)} to bet with.".yellow
     sleep 1
     menu
   when 4
-    puts "please come again!"
+    puts "Please Come Again!".green
     !exit
   else
-    puts "Not a valid entry"
+    puts "Not a valid entry.".red
   end
 end
