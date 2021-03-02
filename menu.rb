dccac2629aedd7de622e1565f1a820364d404b5e
Jared Fitts <jfitts@gmail.com>
2021-03-01 19:58:41 -0700

    created slot game

--- menu.rb
+++ menu.rb
@@ -1,15 +1,8 @@
-# Basic Objectives:
-
-# Start game player has a name and an initial bankroll
-# Player can go to different games via menu
-# Slots
-# High / Low
-# Player places bet and wins / loses (hint: rand)
-# Player's bankroll goes up and down with wins and losses
 require_relative 'highlow'
+require_relative 'slots_menu'
 
 def menu
-  
+
   puts "welcome to the casino. What would you like to do?"
   puts "1) Play Slots?"
   puts "2) Play High Low?"
@@ -18,9 +11,11 @@ def menu
   choice = gets.chomp.to_i
   case choice
   when 1
-    play_slots
+    slots_menu
+    menu
   when 2
     high_low_menu
+    menu
   when 3 
     #bankroll
     puts "You have $#{@amount.to_f.round(3)} to bet with."
---
+++ slots_menu.rb
@@ -0,0 +1,104 @@
+def
+    slot_game
+    
+    @first_slot = rand(1..5)
+    @second_slot = rand(1..5)
+    @third_slot = rand(1..5)
+    border
+    puts "RESULTS"
+    puts
+    puts "#{@first_slot}" "-" "#{@second_slot}" "-" "#{@third_slot}"
+  
+    if @first_slot==@second_slot && @second_slot==@third_slot
+  
+      puts "You Win! Winner Winner Chicken Dinner!" 
+     @amount += @bet * 3
+     puts "You Won #{@bet * 3}"
+     puts "Your updated bankroll is $#{@amount}"
+     play_again  
+    elsif @first_slot==@second_slot || @second_slot==@third_slot || @first_slot==@third_slot
+      
+      puts "Winner Times 2"
+      puts "You Win! Winner Winner Chicken Dinner!" 
+      
+     @amount += @bet * 2
+     puts "You Won #{@bet * 2 }"
+     puts "Your updated bankroll is $#{@amount}"
+     play_again
+    else
+      puts "Shucks, you lose!"
+      @amount -= @bet
+      puts
+      puts "Your updated bankroll is $#{@amount}"
+      puts "loser" 
+    play_again  
+    end
+    end
+  
+    def border
+      puts
+      puts "-" * 40
+      puts
+  end
+  
+    def slots_menu
+      puts
+      puts "************   WELCOME TO SLOTS  ************" 
+      puts
+      puts "If you match 2 numbers payout is times 2" 
+      puts "If you match 3 numbers payout is times 3"
+      puts
+      puts "Please select from below."
+      border
+      puts "1 - Place Your Bet"
+      puts "2 - Return to Casino"
+      border
+      user_input = gets.chomp.to_i
+      case user_input
+      when 1
+        slots_bet
+      when 2
+          puts "Thanks for playing!"
+          menu
+          exit
+      else
+          puts "Invalid, try again."
+          slots_menu
+      end 
+    end
+  
+  def slots_bet
+    @bet = 0
+    puts
+    puts "Here is how much you have $#{@amount}."
+    puts "How much do you want to bet?"
+   
+        @bet = gets.chomp.to_i
+        if @bet > @amount
+            puts "You don't have enough money, please select a lower bet."
+            slots_menu
+  
+        else
+            puts "You have $#{@amount} total, and your bet is $#{@bet}"
+            slot_game
+  
+        end
+  end
+  
+  def play_again
+    puts "Do you want to play again? (1. Yes, 2. No)"
+    choice = gets.chomp.to_i
+    case choice
+    when 1
+        slots_bet
+    when 2
+        "Thanks for playing!"
+        slots_menu
+    end
+  end
+  
+  
+  
+  
+  
+  
